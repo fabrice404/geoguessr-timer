0565145f96f34aa6ea091618fb2b321080c592f6
fabrice404 <fabrice.lamant@gmail.com>
2019-08-10 18:35:32 +0100

    Add build script for Chrome

---
+++ build-chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf dist/chrome*
+mkdir -p dist/chrome/
+cp manifest.chrome.json dist/chrome/manifest.json
+cp -r img/ dist/chrome/img/
+cp -r src/ dist/chrome/src/
+cd dist/
+zip chrome.zip chrome/ -r
